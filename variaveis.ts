999bcc6ae2734cd8d92f128101f6654f95452256
r3tr0boy <r3tr0boy1234@proton.me>
2025-05-27 13:48:55 -0300

    Olá mundo e variaveis

---
+++ variaveis.ts
@@ -0,0 +1,11 @@
+const nome: String = "Osvaldo";
+const idade: Number = 30;
+const altura: Number = 1.80;
+const vivo: Boolean = true;
+const bens: Array<string> = ['carro', 'casa', 'bitcoin']
+
+console.log(nome);
+console.log(idade);
+console.log(altura);
+console.log(vivo);
+console.log(bens)
